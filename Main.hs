d2f7ea9dafc62037351d54c10952b1aa9a57d218
Matthew Pickering <matthewtpickering@gmail.com>
2018-03-20 15:25:37 +0000

    Remove criterion

--- Main.hs
+++ Main.hs
@@ -1,21 +1,17 @@
 module Main where
 
-import Criterion
-import Criterion.Main
 import qualified HandExamples as H
 import qualified GenericLensExamples as GL
 import Logic (bigLogic)
 
 import Control.DeepSeq
 
-logicBench name f = env logicEnv (\h -> bench name $ nf f h)
+{-
+These should be same speed, with the late-specialisation pass, they are.
 
-logicEnv = return bigLogic
+Otherwise GL is about 2x as slow as Hand
+-}
 
-
-main = defaultMain [
-  bgroup "update/logic" [
-      logicBench "hand"  H.updateLogicConst
-      , logicBench "gl"  GL.updateLogicConst
-   ]
- ]
+main =
+--  print $ rnf $ H.updateLogicConst bigLogic
+  print $ rnf $ GL.updateLogicConst bigLogic
